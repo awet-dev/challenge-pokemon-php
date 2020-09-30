b0c2ce6c68a601a3bb0abe2f9a0b774ca96b5348
Awet Tesfay <whitezscope@gmail.com>
2020-09-30 11:23:37 +0200

    get the previous evolution and display it to the page

--- index.php
+++ index.php
@@ -5,28 +5,42 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$myMoves = array();
+$myMoves = array(); // put the selected moves here
 
-if (isset ($_GET['subject'])) {
-    $name_Or_Id = $_GET['subject'];
-    $content = file_get_contents("https://pokeapi.co/api/v2/pokemon/$name_Or_Id", true);
-    $data = json_decode($content, true); // make the data readable
-    $pokSrc =  $data['sprites']['front_shiny'];
-    $pokName = $data['name'];
-    $pokId =  $data['id'];
-
-     foreach ($data['moves'] as $move) {
+if (isset ($_GET['name'])) {
+    $name_Or_Id = $_GET['name'];
+    $pokData = file_get_contents("https://pokeapi.co/api/v2/pokemon/$name_Or_Id", true);
+    $pokData = json_decode($pokData, true); // make the data readable
+    // echo var_dump($pokData);
+    // retrieve the data
+    $pokSrc =  $pokData['sprites']['front_shiny'];
+    $pokName = $pokData['name'];
+    $pokId =  $pokData['id'];
+    foreach ($pokData['moves'] as $move) {
         array_push($myMoves, $move['move']['name']);
     }
-    $output = array_slice($myMoves, 0, 4);
-    $output = implode(" ",$output);
-    echo $output;
+    $output = array_slice($myMoves, 0, 4); // get only 4 elements from the array
+    $output = implode(" ",$output); // change array to string and separate them with space
+
+    $evolution = file_get_contents("https://pokeapi.co/api/v2/pokemon-species/$name_Or_Id", true);
+    $evoData = json_decode($evolution, true);
+    if ($evoData['evolves_from_species'] !== NULL) {
+        $preEvoName = $evoData['evolves_from_species']['name'];
+        $preEvoData = file_get_contents("https://pokeapi.co/api/v2/pokemon/$preEvoName", true);
+        $preEvoData = json_decode($preEvoData, true);
+        $evoPokSrc =  $preEvoData['sprites']['front_shiny'];
+    }
+
 
 } else {
     $pokId = 'write a name or id';
     $pokSrc = '';
     $pokName = '';
     $pokMove = '';
+    $output = '';
+
+    $preEvoName = '';
+    $evoPokSrc = '';
 }
 ?>
 
@@ -41,21 +55,21 @@ if (isset ($_GET['subject'])) {
 </head>
 <body>
     <form name="form" action="" method="get">
-        <input type="text" name="subject" id="subject">
-        <input type="submit" value="submit">
+        <input type="text" name='name' id="name">
+        <input type="submit" value="Search Pokeman">
     </form>
 
     <div class="result">
         <img src="<?php echo $pokSrc ?>">
         <p><?php echo $pokId ?></p>
         <p><?php echo $pokName ?></p>
-        <!--<ul>
-            <li><?php echo $pokMove ?></li>
-            <li><?php echo $pokMove ?></li>
-            <li><?php echo $pokMove ?></li>
-            <li><?php echo $pokMove ?></li>
-        </ul>-->
+        <p><?php echo $output?></p>
     </div>
+    <div class="result">
+        <img src="<?php echo $evoPokSrc ?>">
+        <p><?php echo $preEvoName ?></p>
+    </div>
+
 </body>
 </html>
 

1d1bc0ce7d9ea19645692f0ce6cdd5362ec2bc8a
awet tesfay <whitezscope@gmail.com>
2020-10-09 17:15:28 +0200

    display the base data of evolution species

--- index.php
+++ index.php
@@ -83,22 +83,6 @@ for ($i = 1; $i <= 20; $i++) {
     array_push($pok_name, $data['name']);
 }
 
-// if it is clicked display the detail of that pokemon
-if (isset($_GET['link'])) {
-    $index = $_GET['link'];
-    $id = $pok_id[$index];
-    $move = $moves[$index];
-    $evolve_from_species = get_evolution("https://pokeapi.co/api/v2/pokemon/$id");
-    $color = $evolve_from_species['color'];
-    if($evolve_from_species['species'] !== NULL) {
-        $name_evolve_species = $evolve_from_species['species']['name'];
-        $evolve_data = getData("https://pokeapi.co/api/v2/pokemon/$name_evolve_species");
-        $evolve_img_src = $evolve_data['sprites']['front_shiny'];
-    }
-} else {
-    unset($id);
-}
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -150,13 +134,44 @@ if (isset($_GET['link'])) {
     </div>
 
     <div class="container text-center">
-        <figure class="figure row row-cols-4">
-            <?php foreach ($img_src AS $g => $value) {
+        <div class="row">
+            <?php
+            // if it is clicked display the detail of that pokemon
+
+            if (isset($_GET['link'])) {
+                $index = $_GET['link'];
+                $id = $pok_id[$index];
+                $move = $moves[$index];
+                $evolve_from_species = get_evolution("https://pokeapi.co/api/v2/pokemon/$id");
+                $color = $evolve_from_species['color'];
+                $evolve_img_src = "";
+                $name_evolve_species = "";
+                $evolve_id = "";
+                if($evolve_from_species['species'] !== NULL) {
+                    $name_evolve_species = $evolve_from_species['species']['name'];
+                    $evolve_data = getData("https://pokeapi.co/api/v2/pokemon/$name_evolve_species");
+                    $evolve_img_src = $evolve_data['sprites']['front_shiny'];
+                    $evolve_id = $evolve_data['id'];
+                }
+
+                echo "<div class='card' style='width: 18rem;'>
+                            <img src='$evolve_img_src' class='card-img-top' alt=''>
+                            <div class='card-body'>
+                                <h5 class='card-title'><strong>Name: </strong>$name_evolve_species, <strong>Id: </strong>$evolve_id</h5>
+                                <p class='card-text'><strong>Moves: </strong>$move</p>
+                            </div>
+                       </div>";
+
+            } else {
+                unset($id);
+            }
+    echo "<figure class='figure row row-cols-4'>";
+            foreach ($img_src AS $g => $value) {
                 echo "<div class='col'>
                             <a href='?link=$g'><img src='$value' class='figure-img img-fluid rounded' alt='Responsive image'/></a>
                             <figcaption class='figure-caption'>$pok_name[$g]</figcaption>
-                      </div>";}
-            ?>
+                      </div>";
+            }?>
         </figure>
     </div>
     <!--here start the old code-->

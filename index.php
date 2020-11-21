b4584e11c38e6627371dc77d27fbfbf8ae88dd85
Awet Tesfay <whitezscope@gmail.com>
2020-11-21 16:48:16 +0100

    cleaned and working

--- index.php
+++ index.php
@@ -5,54 +5,58 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
- // put the selected moves here
-$myText = array();
-if (isset($_GET['name'])){
-    $pokemon = $_GET['name'];
-} else {
-    $pokemon = 1;
+function getData($api, $id = null) {
+    $data = file_get_contents($api.$id, true);
+    return json_decode($data, true);
 }
 
-function getData($http) {
-    $get = file_get_contents($http, true);
-    return json_decode($get, true);
+function getSpecies($url, $array = []) {
+    $evolutionChain = getData($url);
+    $evolutionUrl = $evolutionChain['evolution_chain']['url'];
+    $evolution = getData($evolutionUrl);
+    $evolvesTo = $evolution['chain'];
+    // push it to array
+    while ($evolvesTo['evolves_to']) {
+        array_push($array, $evolvesTo['species']['name']);
+        $evolvesTo = $evolvesTo['evolves_to'][0];
+    }
+    array_push($array, $evolvesTo['species']['name']);
+    return $array;
 }
 
-function changArray($data) {
-    $array = array();
-    foreach ($data['moves'] as $move) {
+function getFourMoves($moves, $array = []) {
+    foreach ($moves as $move) {
         array_push($array, $move['move']['name']);
-        if (count($array) === 4) {
+        if (count($array) > 5) {
             break;
         }
     }
-    return implode(" ", $array); // change array to string and separate them with space
+    return implode("", $array);
 }
 
-$url = "https://pokeapi.co/api/v2/pokemon/$pokemon";
-$data = getData($url);
-$output = changArray($data);
-
-$evolution = getData($data['species']['url']);
-/*
-$evolutionChain = getData($evolution['evolution_chain']['url']);
-while ($evolutionChain['chain']['evolves_to']) {
-    $link = $evolutionChain['chain']['evolves_to'][0]['species']['name'];
-    $name = $link;
-    $link += ['evolves_to'][0]['species']['name'];
-    var_dump($evolutionChain);
-}
- */
-$color = $evolution['color']['name'];
-$flavor_text = $evolution['flavor_text_entries'][0]['flavor_text'];
-if ($evolution['evolves_from_species'] !== NULL) {
-    $evolves_from = getData('https://pokeapi.co/api/v2/pokemon/'.$evolution['evolves_from_species']['name']);
+$pokemonApi = "https://pokeapi.co/api/v2/pokemon/";
+$pokemonImg = [];
+$pokemonMoves = "";
+if (!empty($_POST['id'])) {
+    $pokemonData = getData($pokemonApi, $_POST['id']);
+    $speciesName = getSpecies($pokemonData['species']['url']);
+    foreach ($speciesName as $item) {
+        $speciesData = getData($pokemonApi, $item);
+        array_push($pokemonImg, $speciesData['sprites']['front_shiny']);
+    }
+    $pokemonMoves = getFourMoves($pokemonData['moves']);
 } else {
-    $evolves_from['name'] = 'No previous evolution';
-    $evolves_from['sprites']['front_shiny'] = '';
+    $pokemonData = getData($pokemonApi, 1);
+    $speciesName = getSpecies($pokemonData['species']['url']);
+    foreach ($speciesName as $item) {
+        $speciesData = getData($pokemonApi, $item);
+        array_push($pokemonImg, $speciesData['sprites']['front_shiny']);
+    }
+    $pokemonMoves = getFourMoves($pokemonData['moves']);
 }
 
 ?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -60,86 +64,66 @@ if ($evolution['evolves_from_species'] !== NULL) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-<div id="pokedex">
-    <div id="left">
-        <div id="logo"></div>
-        <div id="bg_curve1_left"></div>
-        <div id="bg_curve2_left"></div>
-        <div id="curve1_left">
-            <div id="buttonGlass">
-                <div id="reflect"></div>
-            </div>
-            <div id="miniButtonGlass1"></div>
-            <div id="miniButtonGlass2"></div>
-            <div id="miniButtonGlass3"></div>
-        </div>
-        <div id="curve2_left">
-            <div id="junction">
-                <div id="junction1"></div>
-                <div id="junction2"></div>
-            </div>
-        </div>
-        <div id="screen">
-            <div id="topPicture">
-                <div id="buttontopPicture1"></div>
-                <div id="buttontopPicture2"></div>
-            </div>
-            <div id="picture">
-                <img src="<?php echo $data['sprites']['front_shiny'] ?> "height="170"/>
-            </div>
-            <div id="buttonbottomPicture"></div>
-            <div id="speakers">
-                <div class="sp"></div>
-                <div class="sp"></div>
-                <div class="sp"></div>
-                <div class="sp"></div>
-            </div>
-        </div>
-        <div id="bigbluebutton"></div>
-        <div id="barbutton1"></div>
-        <div id="barbutton2"></div>
-        <div id="cross">
-            <div id="leftcross">
-                <div id="leftT"></div>
-            </div>
-            <div id="topcross">
-                <div id="upT"></div>
-            </div>
-            <div id="rightcross">
-                <div id="rightT"></div>
-            </div>
-            <div id="midcross">
-                <div id="midCircle"></div>
-            </div>
-            <div id="botcross">
-                <div id="downT"></div>
-            </div>
-        </div>
-    </div>
-    <div id="right">
-        <div id="stats" style="background-color: <?php echo $color?>">
-            <strong>Name:</strong> <?php echo $data['name']; ?><br>
-            <strong>Id:</strong> #<?php echo $data['id']; ?><br>
-            <strong>moves:</strong> <?php echo $output; ?><br>
-            <strong id="center">flavor:</strong> <?php echo $flavor_text; ?><br>
-            <strong>Name:</strong> <?php echo $evolves_from['name']; ?><br>
-            <img src="<?php echo $evolves_from['sprites']['front_shiny'] ?>" />
-        </div>
-        <div id="blueButtons2">
-            <form method="get" id="input-form">
-                <input class="input" type="text" name="name" placeholder="Enter Name Or Id">
-                <input class="input" type="submit" value="Search">
+<body class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Pokemon Api</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+            <form action="" method="post" class="form-inline my-2 my-lg-0">
+                <input name="id" class="form-control mr-sm-2" type="text" placeholder="Pokemon" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
-        <div id="bg_curve1_right"></div>
-        <div id="bg_curve2_right"></div>
-        <div id="curve1_right"></div>
-        <div id="curve2_right"></div>
+    </nav>
+
+    <div class="card">
+        <div class="card-body">
+            <div class="d-flex justify-content-between">
+                <div class="pokemon">
+                    <img class="card-img-top" src="<?php echo $pokemonData['sprites']['front_shiny']?>" alt="Card image cap" style="width: 200px">
+                    <h4 class="card-title">
+                        <strong>Name: </strong> <?php echo ucfirst($pokemonData['name'])?>
+                    </h4>
+                    <p class="card-text"><strong>Moves: </strong><?php echo $pokemonMoves?></p>
+                    <p class="card-text">
+                        <strong>Pokemon Id: </strong><?php echo $pokemonData['id']?>
+                    </p>
+                </div>
+                <div class="d-flex">
+                    <?php foreach ($pokemonImg as $img) :?>
+                        <img class="card-img-top" src="<?php echo $img?>" alt="Card image cap" style="width: 100px">
+                    <?php endforeach;?>
+                </div>
+            </div>
+            <div class="d-flex">
+                <form action="" method="post" class="form-inline my-2 my-lg-0 mr-2">
+                    <input name="id" type="hidden" value="<?php echo $pokemonData['id'] > 1? $pokemonData['id']-1: 1?>">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Previous One</button>
+                </form>
+                <form action="" method="post" class="form-inline my-2 my-lg-0">
+                    <input name="id" type="hidden" value="<?php echo $pokemonData['id']+1?>">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Next One</button>
+                </form>
+            </div>
+        </div>
     </div>
-</div>
+
 </body>
 </html>
